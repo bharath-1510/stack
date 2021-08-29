67e7d701fdad59a3c18cab3ab07ff2f106c7cb7e
Bharath <89100001+bharath-1510@users.noreply.github.com>
2021-08-29 11:19:40 +0530

    Add files via upload

---
+++ stack1.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define max 10
+class Stack
+{
+	int top;
+public:
+	int a[max];
+	Stack()
+	{
+		top=-1;
+	}
+	bool isEmpty();	
+	int size();
+	void push(int x);
+	int pop();
+	int top1();
+	void print();
+};
+bool Stack::isEmpty()
+{
+	if(top==-1)
+		return true;
+	else
+		return false;
+}
+void Stack::push(int x)
+{
+	if(top>=max-1)
+		cout<<"Stack is full";
+	else
+	{
+		top++;
+		a[top]=x;		
+	}	
+}
+int Stack::pop()
+{
+	if(isEmpty())
+		cout<<"Stack is Empty";
+	else		
+	{
+		int data =a[top];
+		top--;
+		return data;
+	}
+	return 0;
+}
+int Stack::top1()
+{
+	if(isEmpty())
+		cout<<"Stack is Empty";
+	else		
+	{
+		int data =a[top];
+		return data;
+	}
+	return 0;
+}
+int Stack::size()
+{
+	return (top+1);
+}
+void Stack::print()
+{
+	int t=top;
+	if(t==-1)
+		cout<<"Stack is empty";
+	else
+	{
+	for(int i=0;i<t+1;i++)
+		cout<<a[i]<<endl;	
+	}
+}
+int main()
+{
+	class Stack s;
+	s.push(1);
+	s.push(2);
+	s.push(3);
+	s.print();
+	return 0;
+}
